b1f3bde4772b99e954edbcc9255dd96848be93a1
tlberglund <tlberglund@gmail.com>
2011-03-21 08:44:42 -0600

    Trivial change to HelloWorld to illustrate submodules

--- minimal-java-build-file/src/main/java/org/gradle/example/simple/HelloWorld.java
+++ minimal-java-build-file/src/main/java/org/gradle/example/simple/HelloWorld.java
@@ -2,6 +2,6 @@ package org.gradle.example.simple;
 
 public class HelloWorld {
   public static void main(String args[]) {
-    System.out.println("hello, world");
+    System.out.println("hello, world!");
   }
 }
