02cb9fa6608e2b0794f2bc34645acdff25e3859f
Matthew McCullough <matthewm@ambientideas.com>
2011-03-31 11:26:49 +0200

    Adding Maven gradle comparison with dependencies

---
+++ maven-gradle-comparison-dependency/src/test/java/TestMain.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+
+public class TestMain
+{
+    @Test
+    public void testTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void testFalse()
+    {
+        assertFalse( false );
+    }
+}
