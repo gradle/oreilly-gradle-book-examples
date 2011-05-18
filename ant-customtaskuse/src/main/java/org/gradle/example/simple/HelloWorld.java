e8cb3b501e794b11ef52bb9236578e4e8a7da25a
Matthew McCullough <matthewm@ambientideas.com>
2011-05-18 08:48:25 -0400

    Updated names of PMD and CPD rules and got both examples working

--- ant-customtaskuse/src/main/java/org/gradle/example/simple/HelloWorld.java
+++ ant-customtaskuse/src/main/java/org/gradle/example/simple/HelloWorld.java
@@ -4,4 +4,10 @@ public class HelloWorld {
   public static void main(String args[]) {
     System.out.println("hello, world");
   }
+  
+  public static void doNothing() {
+      //Empty if statement should be disliked by PMD
+      if(true) {
+      }
+  }
 }
