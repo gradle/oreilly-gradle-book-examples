11ed70932cf7277cad73fd3c14d84eac14ce5a0b
Matthew McCullough OnLine Git Guy <matthewm@ambientideas.com>
2011-05-25 15:17:32 -0600

    Added generated tests

---
+++ testing-junit/src/test/java/org/gradle/example/simple/TestPerson972.java
@@ -0,0 +1,22 @@
+package org.gradle.example.simple;
+
+import org.gradle.example.simple.Person;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestPerson972 {
+  @Test
+  public void testPerson() {
+      Person p = new Person();
+      p.setAge(20);
+      p.setName("Fird Birfle");
+      p.setSalary(195750.22);
+      
+      assertEquals(215325.242, p.calculateBonus(), 0.01);
+      assertEquals("The Honorable Fird Birfle", p.becomeJudge());
+      assertEquals(30, p.timeWarp());
+      
+      p.wasteTime();
+  }
+}
