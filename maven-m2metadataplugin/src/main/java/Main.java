0f8c02361be1aff63c35ab4a55169eba019d60c3
Matthew McCullough <matthewm@ambientideas.com>
2011-05-26 17:26:51 -0600

    Gradle m2metadata plugin example

---
+++ maven-m2metadataplugin/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+    private static Logger logger = Logger.getLogger("com.sample");
+    
+    public static void main(String args[]) {
+        logger.warn("Working along with log4j.");
+    }
+}
---
+++ maven-m2metadataplugin/src/test/java/TestMain.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestMain {
+  @Test
+  public void testMain() {
+      Main.main(null);
+  }
+}
