99c481ba665fecaa2457c7abe94d8a1b83ffc4af
Matthew McCullough <matthewm@ambientideas.com>
2011-09-23 00:11:41 -0700

    Added single-test runner addRule

---
+++ hooks-addrule/src/test/java/HelloWorldTest1.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest1 {
+  @Test
+  public void testHelloWorldPrinting1() {
+      HelloWorld.main(null);
+      assertTrue(true);
+  }
+}
---
+++ hooks-addrule/src/test/java/HelloWorldTest2.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest2 {
+  @Test
+  public void testHelloWorldPrinting2() {
+      HelloWorld.main(null);
+      assertTrue(true);
+  }
+}
